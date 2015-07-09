30a1fe46a6dfc24c9c20c6c962493f54ace741c6
Jesús Vallejo <jv3>
2015-07-09 21:13:25 +0200

    Vistas parciales y marco de la alicación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,6 @@
 // GET /quizes/question
 exports.question = function(req, res) {
   res.render('quizes/question', {title: 'Quiz',
-                                 subtitle: 'Quiz: el juego de las preguntas',
                                  pregunta: 'Capital de Italia'});
 };
 
@@ -9,11 +8,9 @@ exports.question = function(req, res) {
 exports.answer = function(req, res) {
   if (req.query.respuesta === 'Roma') {
     res.render('quizes/answer', {title: 'Quiz',
-                                   subtitle: 'Quiz: el juego de las preguntas',
                                    respuesta: 'Correcto'});
   } else {
     res.render('quizes/answer', {title: 'Quiz',
-                                   subtitle: 'Quiz: el juego de las preguntas',
                                    respuesta: 'Incorrecto'});
   }
 };
