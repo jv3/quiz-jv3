1f37e0c399f3354ba39cc936d74b97d057cc9580
Jesús Vallejo <jv3>
2015-07-15 19:06:17 +0200

    Despliegue de la Base de Datos en Heroku

--- models/models.js
+++ models/models.js
@@ -1,13 +1,31 @@
 // Construye la DB y el modelo importando quiz.js
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:password@host:port/DATABASE_URL
+// SQLite   DATABASE_URL = squlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name =   (url[6]||null);
+var user =      (url[2]||null);
+var pwd =       (url[3]||null);
+var protocol =  (url[1]||null);
+var dialect =   (url[1]||null);
+var port =      (url[5]||null);
+var host =      (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite",
-                                                storage: "quiz.sqlite" // fichero donde se van a guardar los datos
-                                              });
+// Usar BBDD SQLite o Postgres:
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect: protocol,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage, // solo SQLite (.env)
+    omitNull: true // solo Postgres
+  }
+);
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
